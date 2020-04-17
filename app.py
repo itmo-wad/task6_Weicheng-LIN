25977b509bd59e7ae6c8c39dc1915af1196ba906
stevenlin510 <steven61413@gmail.com>
2020-04-18 00:23:48 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+
+from markupsafe import escape
+
+app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+@app.route('/index')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return redirect(url_for('login'))
+
+@app.route('/cabinet')
+def cabinet():
+    if 'username' in session:
+        return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        session['psw'] = request.form['psw']
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In')
+
+if __name__ == "__main__":
+    app.run(debug=True)
